d9925f245065364d2ed0f2dc30fbe480aaa38585
Patrick Rubio <patrickr002@gmail.com>
2026-05-02 22:17:31 -0700

    Created LoginResponse.java

---
+++ src/main/java/com/reservenow/auth/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.reservenow.auth;
+
+public class LoginResponse {
+    private String message;
+    private Long userId;
+    private String email;
+
+    public LoginResponse(String message, Long userId, String email) {
+        this.message = message;
+        this.userId = userId;
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
