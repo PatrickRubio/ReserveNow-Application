75da8f3e84073755877759292a08a15ebc3a2b74
Patrick Rubio <patrickr002@gmail.com>
2026-05-06 20:36:27 -0700

    Add LoginRequest for incoming requests for login with email and password

--- src/main/java/com/reservenow/auth/LoginRequest.java
+++ src/main/java/com/reservenow/auth/LoginRequest.java
@@ -1,14 +1,25 @@
 package com.reservenow.auth;
 
-import lombok.Getter;
-import lombok.Setter;
-
+// request body for login
 public class LoginRequest {
-    @Getter
-    @Setter
+
     private String email;
     private String password;
 
     public LoginRequest() {
+    }
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
